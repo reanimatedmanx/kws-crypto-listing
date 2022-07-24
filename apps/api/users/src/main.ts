4645106a8a621e32e8bfda7a04d554de969efb44
reanimated-man <me@reanimatedman.dev>
2022-07-24 16:15:15 +0300

    feat: add swagger API documentation

--- apps/api/users/src/main.ts
+++ apps/api/users/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import {
   FastifyAdapter,
   NestFastifyApplication,
@@ -20,6 +21,16 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3334;
+
+  const config = new DocumentBuilder()
+    .setTitle('Users API')
+    .setDescription('Users API description')
+    .setVersion(process.env.API_VERSION)
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(port, '0.0.0.0');
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
