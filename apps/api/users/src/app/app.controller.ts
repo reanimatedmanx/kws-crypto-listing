f7c6ad72c70c08a7bdb1712851e1ffe80cecb0fb
reanimated-man <me@reanimatedman.dev>
2022-07-24 16:16:17 +0300

    feat: add service versioning and metadata

--- apps/api/users/src/app/app.controller.ts
+++ apps/api/users/src/app/app.controller.ts
@@ -1,8 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 import { AppService } from './app.service';
 
-@Controller('/users')
+@ApiTags('app')
+@Controller({
+  version: process.env.API_VERSION,
+})
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
--- apps/api/users/src/app/app.service.ts
+++ apps/api/users/src/app/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to users!' };
+  getData(): { name: string; version: string } {
+    return { name: 'Users API', version: process.env.API_VERSION };
   }
 }
