b6ad05c762850acda2ccc8cf75139a9517aead11
reanimated-man <me@reanimatedman.dev>
2022-07-25 20:58:53 +0300

    fix: correct field case in prisma schema as per req

--- database/migrations/20220725175727_init/migration.sql
+++ database/migrations/20220725175727_init/migration.sql
@@ -1,11 +1,11 @@
 -- CreateTable
-CREATE TABLE "Instruments" (
+CREATE TABLE "Instrument" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "instrument_symbol" TEXT NOT NULL,
     "instrument_name" TEXT NOT NULL,
     "usd_price" REAL NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
 );
 
 -- CreateTable
@@ -14,8 +14,8 @@ CREATE TABLE "User" (
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "instruments_access" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
 );
 
 -- CreateIndex
