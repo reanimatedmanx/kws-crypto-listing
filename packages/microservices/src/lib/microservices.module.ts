2210a10b6f26aa21973f611a0d2081520a1367a9
reanimated-man <me@reanimatedman.dev>
2022-07-25 15:41:07 +0300

    chore: refactor microservices.module.ts

--- packages/microservices/src/lib/microservices.module.ts
+++ packages/microservices/src/lib/microservices.module.ts
@@ -2,16 +2,17 @@ import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './modules/prisma/prisma.module';
 
+const publicModules = [
+  PrismaModule,
+  ConfigModule.forRoot({
+    envFilePath: ['.env.development', '.env.test', '.env', '.env.prod'],
+  }),
+];
 @Global()
 @Module({
-  imports: [
-    PrismaModule,
-    ConfigModule.forRoot({
-      envFilePath: ['.env.development', '.env.test', '.env', '.env.prod'],
-    }),
-  ],
+  imports: [...publicModules],
   controllers: [],
   providers: [],
-  exports: [PrismaModule, ConfigModule],
+  exports: [...publicModules],
 })
 export class MicroservicesModule {}
