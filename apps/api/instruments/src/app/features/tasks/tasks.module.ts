db6797bf87b78dcb12a0d0e0b7624b8da49c67dc
reanimated-man <me@reanimatedman.dev>
2022-07-25 19:33:19 +0300

    feat: get instruments data from 3rd party API

--- apps/api/instruments/src/app/features/tasks/tasks.module.ts
+++ apps/api/instruments/src/app/features/tasks/tasks.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MicroservicesModule } from '@kwc/microservices';
 import { ScheduleModule } from '@nestjs/schedule';
+import { HttpModule } from '@nestjs/axios'
 
 @Module({
-  imports: [MicroservicesModule, ScheduleModule.forRoot()],
+  imports: [MicroservicesModule, HttpModule, ScheduleModule.forRoot()],
   controllers: [],
   providers: [TasksService, ConfigService],
 })
--- apps/api/instruments/src/app/features/tasks/tasks.service.ts
+++ apps/api/instruments/src/app/features/tasks/tasks.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Cron } from '@nestjs/schedule';
-
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 /**
  * A barebones task service.
  * A much more sophisticated way of implementing this would be to make it unaware of what it should execute to make it trully generic.
@@ -10,13 +11,37 @@ import { Cron } from '@nestjs/schedule';
 export class TasksService {
   private readonly logger = new Logger(TasksService.name);
 
-  constructor(private configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly httpService: HttpService
+  ) {}
 
   @Cron('*/5 * * * * *')
   async handleFetchCoins() {
+    const startTime = Date.now();
     const uri = this.configService.get('API_EXTERNAL_COINGECKO');
 
-    // TODO Add a queue (ex bull) to avoid race conditions
-    this.logger.debug(`Fetching ${uri}`);
+    let shouldUpdateDb = false;
+
+    try {
+      this.logger.debug(`Fetching ${uri}...`);
+
+      const stream$ = this.httpService.get(uri);
+      const res = await firstValueFrom(stream$);
+      const endTime = Date.now();
+      const totalTimeMs = endTime - startTime;
+      shouldUpdateDb = !!res.data.length;
+
+      this.logger.log(
+        `Fetched ${res.data.length} entries successfully in ${totalTimeMs}ms.`
+      );
+    } catch (err) {
+      this.logger.debug(`Fetching ${uri} failed. Skipping update.`);
+      this.logger.error(err);
+    }
+
+    if (shouldUpdateDb) {
+      // TODO the DB update goes here.
+    }
   }
 }
